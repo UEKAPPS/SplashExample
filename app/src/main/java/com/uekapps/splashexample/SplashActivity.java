f42456d5b41d5b969283caacf5cd5dd2a5307c14
Ufuk Enes Kurt <83250239+UEKAPPS@users.noreply.github.com>
2023-09-25 13:32:57 +0300

    Update SplashActivity.java

--- app/src/main/java/com/uekapps/splashexample/SplashActivity.java
+++ app/src/main/java/com/uekapps/splashexample/SplashActivity.java
@@ -19,6 +19,7 @@ public class SplashActivity extends AppCompatActivity {
         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
         | View.SYSTEM_UI_FLAG_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         //Thread
